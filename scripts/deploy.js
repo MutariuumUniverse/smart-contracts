99c07e97e9256a8af55dad4c40381cd35b528ddb
Mouradif <mouradif@gmail.com>
2022-12-12 19:00:58 +0100

    Deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,22 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const signer = new ethers.Wallet(process.env.SIGNER_KEY);
+  const errors = [];
+  if (!process.env.SIGNER_ADDRESS) {
+    errors.push('SIGNER_ADDRESS must be set in your env file');
+  }
+  if (!process.env.STAKING_ADDRESS) {
+    errors.push('STAKING_ADDRESS must be set in your env file');
+  }
+  if (errors.length > 0) {
+    throw new Error(errors.map(e => `\n\t- ${e}`).join(''));
+  }
   const Land = await ethers.getContractFactory("MutariuumLand");
-  const land = await Land.deploy(signer.address, signer.address);
+  const land = await Land.deploy(process.env.SIGNER_ADDRESS, process.env.STAKING_ADDRESS);
 
   await land.deployed();
 
-  console.log('Deployed land', land.address);
+  console.log(`Lands deployed at address ${land.address}`);
 }
 
 main().catch((error) => {
